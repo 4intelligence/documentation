901e4547dd06785052527952b1d8526c795ed025
Nathalya Stefhany <nathalya.stefhany@hotmail.com>
2023-07-03 13:59:29 -0300

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,12 @@
+export const menu = [
+  {
+    name: "Getting Started",
+    path: "getting-started",
+    submenu: [
+      {
+        name: "What is No-Code AI?",
+        path: "no-code-ai.md",
+      }
+    ]
+  }
+]
